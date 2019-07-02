6712cbadd7246f7bf7bc39fbe7f8cae8c3040f0f
MarcRochkind <rochkind@basepath.com>
2019-07-02 15:14:06 -0600

    procedures are now recursive; other bugs fixed

--- pl0compiler.c
+++ pl0compiler.c
@@ -4,6 +4,28 @@
 	(basepath.com/701). Otherwise, it's a standalong console program.
 
 	Written by Marc Rochkind, June 2019. Released into the public domain.
+
+		program = block "." .
+
+		block = [ "const" ident "=" number {"," ident "=" number} ";"]
+				[ "var" ident {"," ident} ";"]
+				{ "procedure" ident ";" block ";" } statement .
+
+		statement = [ ident ":=" expression | "call" ident
+					  | "print" expression
+					  | "begin" statement {";" statement } "end"
+					  | "if" condition "then" statement
+					  | "while" condition "do" statement ].
+
+		condition = "odd" expression |
+					expression ("="|"#"|"<"|"<="|">"|">=") expression .
+
+		expression = [ "+"|"-"] term { ("+"|"-") term}.
+
+		term = factor {("*"|"/") factor}.
+
+		factor = ident | number | "(" expression ")".
+
 */
 
 #ifdef _MSC_VER
@@ -15,6 +37,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "debug.h"
 
 #ifdef EMULATOR
 #include "pl0compiler.h"
@@ -35,13 +58,16 @@ static int labelcounter = 0;
 static int tempcounter = 0;
 static int maxtemp = 0;
 static int blocknum = 0;
+static int blocklevel = 0;
 
 typedef enum {blocktype = 1, vartype = 2, consttype = 4, proctype = 8} SYMTYPE;
 static int numsyms;
-struct {
+struct SYMTABLE {
 	int blocknum;
 	SYMTYPE symtype;
 	char symname[SYMLEN];
+	int blocklevel;
+	int position;
 } symtable[SYMTABLELEN];
 
 static void nextsym(bool init);
@@ -49,8 +75,6 @@ static bool error(const char msg[], const char* param1, const char* param2);
 
 static void expression(void);
 static void program(void);
-static void code(const char* label, const char* s1, const char* s2);
-static void code2(const char* label, const char* s1, const char* s2);
 
 #ifndef EMULATOR
 
@@ -71,6 +95,62 @@ int main(int argc, char *argv[])
 
 #endif
 
+static void code(const char* label, const char* s1, const char* s2)
+{
+	fprintf(out, "%s\t%s%s # @%d\n", label, s1, s2, linenum);
+}
+
+static void code2(const char* label, const char* s1, const char* s2)
+{
+	fprintf(out2, "%s\t%s%s # @%d\n", label, s1, s2, linenum);
+}
+
+static void codepush(const char* addr, const char *comment)
+{
+	code("", "-loadmq ", addr);
+	code("", "radd *", "");
+	code("", "tr .push", comment);
+}
+
+static const char* literal(n)
+{
+	static char buf[100];
+
+	snprintf(buf, sizeof(buf), "=%d", n);
+	return buf;
+}
+
+static void codepushn(int n)
+{
+	codepush(literal(n), "");
+}
+
+// putss addr of frame ptr in addr part of ac
+// next instr should be storea (e.g., "storea *+1")
+static code_getframeptraddr(int blocklevel)
+{
+	code("", "-radd =@.levels", "");
+	code("", "-add ", literal(blocklevel * 2));
+	code("", "aleft 18", "");
+}
+
+// puts addr of frame (autos) in ac
+static code_getframeptr(int blocklevel)
+{
+	code_getframeptraddr(blocklevel);
+	code("", "storea *+1", "");
+	code("", "-radd 0", ""); // addr of frame in ac
+}
+
+// puts addr of stack word in addr part of ac
+// next instr should be storea (e.g., "storea *+2")
+static code_getstackaddr(struct SYMTABLE* st)
+{
+	code_getframeptr(st->blocklevel);
+	code("", "-add ", literal(st->position * 2));
+	code("", "aleft 18", "");
+}
+
 int compile_pl0(const char* path, char* ebuf, int ebuf_size)
 {
 	char outpath[_MAX_PATH];
@@ -84,6 +164,7 @@ int compile_pl0(const char* path, char* ebuf, int ebuf_size)
 	tempcounter = 0;
 	maxtemp = 0;
 	blocknum = 0;
+	blocklevel = -1;
 	numsyms = 0;
 	nextsym(true);
 	in = fopen(path, "r");
@@ -104,7 +185,7 @@ int compile_pl0(const char* path, char* ebuf, int ebuf_size)
 	out2 = fopen(outpath, "w+");
 	if (out2 == NULL)
 		return error("Can't open out2.a", NULL, NULL);
-	code("# self-load.a, print.a, and print-number.a are supplied with the IBM 701 emulator", "", "");
+	code("# self-load.a, stack.a, print.a, and print-number.a are supplied with the IBM 701 emulator", "", "");
 	code("# See basepath.com/701", "", "");
 	code("", "incl self-load.a", "");
 	program();
@@ -124,7 +205,7 @@ typedef enum {
 	endsym, ifsym, whilesym, becomes, thensym, dosym, constsym, comma,
 	varsym, procsym, period, oddsym, printsym, lastsym
 } Symbol;
-Symbol sym;
+static Symbol sym;
 
 static const char *string_symbol[] = {
 	"none", "ident", "number", "lparen", "rparen", "times", "slash", "plus",
@@ -143,7 +224,7 @@ const char* make_symname(int n) {
 	return "?";
 }
 
-static const char *newsymbol(SYMTYPE symtype, const char* symname) {
+static const char *newsymbol(SYMTYPE symtype, const char* symname, int level, int pos) {
 	if (numsyms >= SYMTABLELEN) {
 		error("Too many symbols", NULL, NULL);
 		return "?";
@@ -152,31 +233,34 @@ static const char *newsymbol(SYMTYPE symtype, const char* symname) {
 		symtable[numsyms].blocknum = blocknum;
 		symtable[numsyms].symtype = symtype;
 		strcpy(symtable[numsyms].symname, symname);
+		symtable[numsyms].blocklevel = level;
+		symtable[numsyms].position = pos;
 		numsyms++;
 		return make_symname(numsyms - 1);
 	}
 }
 
-static const char* findsym(const char* x, int types) {
+static const char* findsym(const char* x, int types, struct SYMTABLE **st) {
 	for (int i = numsyms - 1; i >= 0; i--) {
 		if (strcmp(x, symtable[i].symname) == 0) {
-			if ((types & symtable[i].symtype) == symtable[i].symtype)
+			if ((types & symtable[i].symtype) == symtable[i].symtype) {
+				if (st != NULL)
+					*st = &symtable[i];
 				return make_symname(i);
+			}
 		}
 	}
 	error("Symbol not declared at all or not as correct identifier type", x, NULL);
 	return NULL;
 }
 
-static void removescope() {
-	if (numsyms > 0) {
-		int bnum = symtable[numsyms - 1].blocknum;
-		int i;
-		for (i = numsyms - 1; i >= 0 && symtable[i].blocknum == bnum; i--)
-			;
-		if (i >= 0)
-			numsyms = i + 1;
-	}
+static int markscope()
+{
+	return numsyms;
+}
+
+static void removescope(int scope) {
+	numsyms = scope;
 }
 
 static const char *symbol_to_string(Symbol s)
@@ -221,7 +305,12 @@ static int expect(Symbol s) {
 
 static void factor(void) {
 	if (accept(ident)) {
-		code("", "-radd ", findsym(accepted_symvalue, vartype | consttype));
+		struct SYMTABLE *st;
+
+		findsym(accepted_symvalue, vartype | consttype, &st);
+		code_getstackaddr(st);
+		code("", "storea *+1", "");
+		code("", "-radd 0", ""); // value of var/const in ac
 	}
 	else if (accept(number)) {
 		code("", "-radd =", accepted_symvalue);
@@ -266,7 +355,7 @@ static void term(void) {
 
 static void expression(void) {
 	Symbol sign = none;
-	char temp[LABELLEN];
+	char temp[LABELLEN], temp2[LABELLEN];
 
 	maketemp(temp);
 	if (sym == plus || sym == minus) {
@@ -285,8 +374,13 @@ static void expression(void) {
 		term();
 		if (sign == plus)
 			code("", "-add ", temp);
-		else
-			code("", "-sub ", temp);
+		else {
+			maketemp(temp2);
+			code("", "-store ", temp2);
+			code("", "-radd ", temp);
+			code("", "-sub ", temp2);
+			releasetemp();
+		}
 	}
 	releasetemp();
 }
@@ -356,8 +450,11 @@ static void statement(void) {
 	tempcounter = 0;
 	if (accept(ident)) {
 		char v[SYMLEN];
+		char temp[LABELLEN];
+		struct SYMTABLE* st;
 
-		const char* sm = findsym(accepted_symvalue, vartype);
+		maketemp(temp);
+		const char* sm = findsym(accepted_symvalue, vartype, &st);
 		if (sm == NULL) {
 			error("Identifier not declared as var", accepted_symvalue, NULL);
 			nextsym(false);
@@ -366,11 +463,16 @@ static void statement(void) {
 		strcpy(v, sm);
 		expect(becomes);
 		expression();
-		code("", "-store ", v);
+		code("", "-store ", temp);
+		code_getstackaddr(st);
+		code("", "storea *+2", "");
+		code("", "-radd ", temp); // value of rhs in ac
+		code("", "-store 0", ""); // store it onto stack
+		releasetemp();
 	}
 	else if (accept(callsym)) {
 		expect(ident);
-		const char* proc = findsym(accepted_symvalue, proctype);
+		const char* proc = findsym(accepted_symvalue, proctype, NULL);
 		code("", "radd *", "");
 		code("", "tr ", proc);
 	}
@@ -412,53 +514,105 @@ static void statement(void) {
 	}
 }
 
-static void block(void) {
+static int block(void) {
 	char startlabel[LABELLEN];
+	int stack_position = 0;
 
+	blocklevel++;
 	makelabel(startlabel);
 	blocknum++;
-	newsymbol(blocktype, "");
-	code("", "tr ", startlabel);
+	int scope = markscope();
 	if (accept(constsym)) {
 		do {
 			char s[SYMLEN];
+			char buf[100];
 
 			expect(ident);
 			strcpy(s, accepted_symvalue);
 			expect(eql);
 			expect(number);
-			const char* sm = newsymbol(consttype, s);
-			code2(sm, "word ", accepted_symvalue);
+			const char* sm = newsymbol(consttype, s, blocklevel, stack_position);
+			snprintf(buf, sizeof(buf), "=%s", accepted_symvalue);
+			codepush(buf, " # push constant");
+			stack_position++;
 		} while (accept(comma));
 		expect(semicolon);
 	}
 	if (accept(varsym)) {
 		do {
 			expect(ident);
-			const char *s = newsymbol(vartype, accepted_symvalue);
+			const char *s = newsymbol(vartype, accepted_symvalue, blocklevel, stack_position);
 			code2(s, "word", "");
+			codepush("=0", " # push var");
+			stack_position++;
 		} while (accept(comma));
 		expect(semicolon);
 	}
+	code("", "tr ", startlabel); // go around procs
 	while (accept(procsym)) {
-		char rtnlabel[LABELLEN];
 
 		expect(ident);
-		const char *s = newsymbol(proctype, accepted_symvalue);
-		makelabel(rtnlabel);
+		const char *s = newsymbol(proctype, accepted_symvalue, blocklevel, 0);
 		code(s, "add =2", "");
-		code("", "storea ", rtnlabel);
+		code("", "aleft 6", ""); // get 12 addr bits only
+		code("", "aright 6", "");
+		code("", "lright 53", ""); // into mq (53 = 35 + 18)
+		code("", "radd *", "");
+		code("", "tr .push", " # push rtn addr"); // onto stack
+
+		code_getframeptr(blocklevel + 1); // frame ptr for this level in ac
+		code("", "lright 35", "");
+		code("", "radd *", "");
+		code("", "tr .push", " # push prev frame ptr"); // prev frame ptr onto stack
+
+		code_getframeptraddr(blocklevel + 1);
+		code("", "storea *+3", "");
+		code("", "-radd .stx", ""); // set frame ptr to stack addr of first auto
+		code("", "-add =2", "");
+		code("", "-store 0", " # set new frame ptr to autos");
+
 		expect(semicolon);
-		block();
+		int numautos = block(); // will push autos
 		expect(semicolon);
-		code(rtnlabel, "tr", "");
+
+		// Restore frame ptr for this level
+		// Construct bogus SYMTABLE entry for prev frame ptr
+		char temp[LABELLEN];
+		struct SYMTABLE st;
+		st.blocklevel = blocklevel + 1;
+		st.position = -1;
+		code("", "noop # restore frame ptr", "");
+		code_getstackaddr(&st);
+		code("", "storea *+1", "");
+		code("", "-radd 0", ""); // prev frame ptr in ac
+		maketemp(temp);
+		code("", "-store ", temp); // save it
+		code_getframeptraddr(blocklevel + 1); // addr of frame ptr in levels array
+		code("", "storea *+2", "");
+		code("", "-radd ", temp); // get prev frame ptr
+		code("", "-store 0", ""); // put it in levels array
+
+		code("", "-radd ", literal(numautos * 2 + 4)); // total amount to be popped in half words
+		code("", "tr .trpop", "");
+		releasetemp();
 	}
 	code(startlabel, "noop", "");
 	statement();
-	removescope();
+	removescope(scope);
+	blocklevel--;
+	return stack_position;
 }
 
 static void program(void) {
+	//codepush("=0", "");					// rtn addr for level 0 (globals)
+	//codepush("=0", "");					// prev frame ptr for this level
+	// stx + 2 after above push goes into level 0 ptr
+	code("", "-radd =@.levels", "");
+	code("", "aleft 18", "");
+	code("", "storea *+3", "");
+	code("", "-radd .stx", "");
+	code("", "-add =2", "");
+	code("", "-store 0", "");
 	nextsym(false);
 	block();
 	expect(period);
@@ -471,6 +625,7 @@ static void program(void) {
 	}
 	code("", "incl print.a", "");
 	code("", "incl print-number.a", "");
+	code("", "incl stack.a", "");
 	printf("Compiled OK\n");
 }
 
@@ -519,6 +674,8 @@ static void nextsym(bool init)
 				while ((c = fgetc(in)) != '*' && c != EOF)
 					if (c == '\n')
 						linenum++;
+				if (c == EOF)
+					break;
 				if (c == '*' && (c = fgetc(in)) == '/') {
 					c = 0;
 					nextsym(false);
@@ -640,14 +797,4 @@ static bool error(const char msg[], const char* param1, const char* param2)
 		}
 	}
 	return false;
-}
-
-void code(const char* label, const char* s1, const char* s2)
-{
-	fprintf(out, "%s\t%s%s # @%d\n", label, s1, s2, linenum);
-}
-
-void code2(const char* label, const char* s1, const char* s2)
-{
-	fprintf(out2, "%s\t%s%s # @%d\n", label, s1, s2, linenum);
 }
